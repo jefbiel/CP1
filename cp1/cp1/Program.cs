edad8d0f0b615bc60f9ba40745913b69e7f9c6f3
jefbiel <143344048+jefbiel@users.noreply.github.com>
2025-09-08 11:04:17 -0300

    Add files via upload

---
+++ cp1/cp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool encerrar = false;
+
+        while (!encerrar)
+        {
+            Console.WriteLine("1 Somar");
+            Console.WriteLine("2 Subtrair");
+            Console.WriteLine("3 Multiplicar");
+            Console.WriteLine("4 Dividir");
+            Console.WriteLine("0 Sair");
+            Console.Write("Escolha: ");
+
+            int opcao = Convert.ToInt32(Console.ReadLine());
+
+            if (opcao == 0)
+            {
+                encerrar = true;
+                Console.WriteLine("Encerrando...");
+                continue;
+            }
+
+            Console.Write("Digite o primeiro número: ");
+            float x = float.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            float y = float.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+
+                    Console.WriteLine("Resultado: " + (x + y));
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("Resultado: " + (x - y));
+                    break;
+
+                case 3:
+
+                    Console.WriteLine("Resultado: " + (x * y));
+                    break;
+
+                case 4:
+
+                    if (y == 0)
+                        Console.WriteLine("Erro: divisão por zero não permitida!");
+                    else
+                        Console.WriteLine("Resultado: " + (x / y));
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+        }
+    }
+}
+
+
+
+
+
+
